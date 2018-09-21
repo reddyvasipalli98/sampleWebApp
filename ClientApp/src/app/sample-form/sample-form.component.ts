747955384cf4233381542cbf35fd76e8656d20e0
Karthik Vasipalli <Karthik.Vasipalli@abcsupply.com>
2018-09-21 10:40:26 -0500

    Added changes to mock creating various rows of similar data.

--- ClientApp/src/app/sample-form/sample-form.component.ts
+++ ClientApp/src/app/sample-form/sample-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormArray, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-sample-form',
@@ -11,7 +11,7 @@ export class SampleFormComponent {
   constructor(private dataForm: FormBuilder) { }
 
   stateList = ["AL", "AR", "AZ", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"];
-
+  creditCard = new FormArray([new FormControl('')]);
   userData = this.dataForm.group({
     firstName: [''],
     lastName: [''],
@@ -20,9 +20,17 @@ export class SampleFormComponent {
       city: [''],
       state: [''],
       zip: ['']
-    })
+    }),
+    creditCards: this.creditCard
   });
 
+  get creditCards() {
+    return this.userData.get('creditCards') as FormArray;
+  }
+
+  addCreditCards() {
+    this.creditCards.push(this.dataForm.control(''));
+  }
 
   onSubmit() {
     console.warn(this.userData.value);
